6c19ad569ee806f36640d3dc1274a48c66085596
Justin Headley <justin@scal.io>
2017-02-15 21:20:42 -0800

    Added missing cors requirements.

--- manifest.js
+++ manifest.js
@@ -4,39 +4,40 @@ const Config = require('./config');
 
 
 const criteria = {
-    env: process.env.NODE_ENV
+  env: process.env.NODE_ENV
 };
 
 
 const manifest = {
-    $meta: 'This file defines the server.',
-    server: {
-        debug: {
-            request: ['error']
-        },
-        connections: {
-            routes: {
-                security: true
-            }
-        }
+  $meta: 'This file defines the server.',
+  server: {
+    debug: {
+      request: ['error']
     },
-    connections: [{
-        port: Config.get('/port')
-    }],
-    registrations: [
-        {
-            plugin: 'hapi-auth-jwt2'
-        },
-        {
-            plugin: './server/mailer'
-        },
-        {
-            plugin: './server/auth'
-        },
-        {
-            plugin: './server/api'
-        }
-    ]
+    connections: {
+      routes: {
+        security: true,
+        cors: true
+      }
+    }
+  },
+  connections: [{
+    port: Config.get('/port')
+  }],
+  registrations: [
+    {
+      plugin: 'hapi-auth-jwt2'
+    },
+    {
+      plugin: './server/mailer'
+    },
+    {
+      plugin: './server/auth'
+    },
+    {
+      plugin: './server/api'
+    }
+  ]
 };
 
 
@@ -45,11 +46,11 @@ const store = new Confidence.Store(manifest);
 
 exports.get = function (key) {
 
-    return store.get(key, criteria);
+  return store.get(key, criteria);
 };
 
 
 exports.meta = function (key) {
 
-    return store.meta(key, criteria);
+  return store.meta(key, criteria);
 };
