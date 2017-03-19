1c437fd76e8a304110a972cd3a42409558baa556
Justin Headley <justin@scal.io>
2017-03-18 23:13:50 -0700

    Previous commit fixes #4
    Former-commit-id: 4904ed8b4fe4a61625931c64681d95041821d3d1
    Former-commit-id: 6edb851a8081b1276e0b34f7e9b5963ff3a3a462

--- server/api/register.js
+++ server/api/register.js
@@ -172,7 +172,7 @@ module.exports = function (server, mongoose, logger) {
 
     var headersValidation = Joi.object({
       'authorization': Joi.string()
-    }).options({allowUnknown: true});
+    }).options({allowUnknown: true}); 
 
     const optionalAuth = authStrategy === false ? false : { mode: 'try', strategy: authStrategy };
 
