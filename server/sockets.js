f7407b4159b4a3afc0f6159ee35400ac3fae47f9
Justin Headley <justin@scal.io>
2018-01-13 11:40:04 -0800

    Updated chat

--- server/sockets.js
+++ server/sockets.js
@@ -17,14 +17,40 @@ exports.register = function (server, options, next) {
           strategy: authStrategy
         }
       },
+      // auth: false,
+      headers: ['*'],
       onConnection: function (socket) {
-        console.log("connection established:", socket.auth)
+        console.log("connection established", socket.auth)
+        if (!socket.auth.isAuthenticated) {
+          console.log("NO AUTH, DISCONNECTING")
+          socket.disconnect()
+        }
+      },
+      onDisconnection: function (socket) {
+        console.log("connection closed")
       }
     }
   }, function (err) {
     if (err) {
       console.error('Failed to load plugin:', err);
     }
+
+    // server.subscription('/conversation/{_id}', {
+    //   filter: function (path, message, options, next) {
+    //     console.debug("PUBLISHED PATH:", path)
+    //     console.debug("PUBLISHED MESSAGE:", message)
+    //     console.debug("PUBLISHED PARAMS:", options.params)
+    //     next(true);
+    //   },
+    //   auth: false,
+    //   onSubscribe: function (socket, path, params, next) {
+    //     console.debug("SUBSCRIPTION PATH:", path)
+    //     console.debug("SUBSCRIPTION PARAMS:", params)
+    //     next();
+    //   }
+    // });
+
+    console.log("WEBSOCKETS REGISTERED")
 
     next();
   });
