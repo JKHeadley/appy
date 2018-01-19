88f31ca3ebb93f4ca1f2f67301d2f599a70c0a5e
Justin Headley <justin@scal.io>
2018-01-19 14:12:45 -0800

    - Added more group functionality to chat. - Fixed default profile image bug.

--- server/models/conversation.model.js
+++ server/models/conversation.model.js
@@ -1,11 +1,19 @@
 'use strict';
 
 const RestHapi = require('rest-hapi');
+const _ = require('lodash');
+const Config = require('../../config');
+
+const USER_ROLES = Config.get('/constants/USER_ROLES');
 
 module.exports = function (mongoose) {
   var modelName = "conversation";
   var Types = mongoose.Schema.Types;
   var Schema = new mongoose.Schema({
+    name: {
+      type: Types.String,
+      description: 'The name of the chat.'
+    },
     lastMessage: {
       type: Types.ObjectId,
       ref: "message"
@@ -15,6 +23,9 @@ module.exports = function (mongoose) {
   Schema.statics = {
     collectionName:modelName,
     routeOptions: {
+      routeScope: {
+        createScope: _.values(USER_ROLES)
+      },
       policies: {
       },
       associations: {
@@ -25,6 +36,12 @@ module.exports = function (mongoose) {
         users: {
           type: "_MANY",
           model: "user",
+        },
+        userData: {
+          type: "MANY_MANY",
+          alias: "user-data",
+          model: "user",
+          linkingModel: "user_conversation"
         },
         messages: {
           type: "ONE_MANY",
