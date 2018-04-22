b42fe3aa1f3c4f693056f48500ef58728024f7f2
Justin Headley <justin@scal.io>
2018-04-22 15:39:02 -0700

    - Added dashboard GitHub banner. - Updated login page to support pre-filled inputs based on url queries.
    Former-commit-id: c2db54235d975a73b2f2fd6c8d2298947863f484
    Former-commit-id: 75afec4c4a60793694b32325ee9cd331542ef586

--- frontend/src/components/views/dashboard/Dashboard.vue
+++ frontend/src/components/views/dashboard/Dashboard.vue
@@ -9,6 +9,15 @@
         <!--links instead.</p>-->
     <!--</div>-->
 
+    <div class="alert alert-success alert-dismissible">
+      <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+      <h4><i class="icon fa fa-check"></i> appy is open source!</h4>
+      Click on the icon to check it out on GitHub
+      <a href="https://github.com/JKHeadley/appy" target="_blank"><i class="fa fa-github fa-2x"></i></a>
+      and please leave a
+      <a href="https://github.com/JKHeadley/appy" target="_blank"><i class="fa fa-star fa-2x"></i></a>!
+    </div>
+
     <!-- Main row -->
     <div class="row">
       <!-- Left col -->
