62d9526d288ed56582bec8c72a17807569399319
Justin Headley <headley.justin@gmail.com>
2018-06-22 00:15:09 -0700

    - Add npm start - Update frontend port - Update readme

--- frontend/test/e2e/specs/test.js
+++ frontend/test/e2e/specs/test.js
@@ -4,7 +4,7 @@
 module.exports = {
   'default e2e tests': function(browser) {
     browser
-      .url('http://localhost:8080')
+      .url('http://localhost:3000')
       .waitForElementVisible('#app', 5000)
       .assert.elementPresent('.logo')
       .assert.containsText('h1', 'DASHBOARD Overview of environment')
